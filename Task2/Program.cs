27114f20350f2c9f3912e8a88a6eb3f10d00145b
Subtype3860 <booravoi@gmail.com>
2022-11-22 21:44:31 +0300

    Final Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Task2;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Укажите путь до директории");
+        var p = Console.ReadLine();
+        var d = new DirectoryInfo(p);
+        Console.WriteLine($"Объём директории {DirSize(d)}");
+    }
+    
+    public static long DirSize(DirectoryInfo d) 
+    {    
+        long size = 0;    
+        // Размер файла.
+        var fis = d.GetFiles();
+        foreach (var fi in fis) 
+        {      
+            size += fi.Length;    
+        }
+        // Размер папки.
+        var dis = d.GetDirectories();
+        foreach (DirectoryInfo di in dis) 
+        {
+            size += DirSize(di);   
+        }
+        return size;  
+    }
+}
