b1e872fb4014adbb91c8397a4ad5d0ea8c502c1b
Subtype3860 <booravoi@gmail.com>
2022-11-22 22:01:34 +0300

    Final Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,26 +4,49 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Укажите путь до директории");
-        var p = Console.ReadLine();
-        var d = new DirectoryInfo(p);
-        Console.WriteLine($"Объём директории {DirSize(d)}");
+
+        int x;
+        do
+        {
+            Console.WriteLine("Укажите путь до директории");
+            var p = Console.ReadLine();
+            var d = new DirectoryInfo(p!);
+            if (d.Exists)
+            {
+                x = 1;
+                Console.WriteLine($"Объём директории {DirSize(d)}");
+            }
+            else
+            {
+                x = 0;
+                Console.WriteLine("Неверно указан путь до директории");
+            }
+        } while (x == 0);
+
     }
     
     public static long DirSize(DirectoryInfo d) 
     {    
-        long size = 0;    
-        // Размер файла.
-        var fis = d.GetFiles();
-        foreach (var fi in fis) 
-        {      
-            size += fi.Length;    
-        }
-        // Размер папки.
-        var dis = d.GetDirectories();
-        foreach (DirectoryInfo di in dis) 
+        long size = 0;
+        try
         {
-            size += DirSize(di);   
+            // Размер файла.
+            var fis = d.GetFiles();
+            foreach (var fi in fis) 
+            {      
+                size += fi.Length;    
+            }
+            // Размер папки.
+            var dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis) 
+            {
+                size += DirSize(di);   
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
         }
         return size;  
     }
