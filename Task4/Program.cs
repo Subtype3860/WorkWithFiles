b38b2b274e28a038f94ef7ce918d3375c5cb6740
Subtype3860 <booravoi@gmail.com>
2022-11-27 15:24:40 +0300

    Start Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Task4;
+
+class Program
+{
+    public static void Main()
+    {
+        Reader(null);
+    }
+
+    /// <summary>
+    /// Чтение бинарного файла
+    /// </summary>
+    /// <param name="path">Путь до бинарного файла</param>
+    private static async void Reader(string? path)
+    {
+        var student = new List<Student>();
+        var p = path ?? "Students.dat";
+
+        using (var s = new BinaryReader(File.Open(p, FileMode.Open), Encoding.UTF8))
+        {
+            while (s.PeekChar() > -1)
+            {
+                var ss = s.Read();
+                string dd = s.ReadString();
+                Console.WriteLine($"{ss}  {dd}");
+
+            }
+        }
+
+        Console.ReadKey();
+
+    }
+}
---
+++ Task4/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task4;
+
+public class Student
+{
+    public string? Name { get;}
+    public string? Group { get;}
+    public DateTime DateOfBirth { get;}
+
+    public Student(string name, string group, DateTime dateOfBirth)
+    {
+        Name = name;
+        Group = group;
+        DateOfBirth = dateOfBirth;
+    }
+}
